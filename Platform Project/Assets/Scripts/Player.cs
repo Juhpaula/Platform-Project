cc3d6318d0941c72db79fec7cdb7fc114813711b
Juhpaula <julliapaula7@gmail.com>
2023-04-11 21:49:29 -0300

    player creation
    Criação do cenário
    Criação do player
    Inicio do código de movimentação do player
    Criação de animações do player

---
+++ Platform Project/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public FixedJoystick moveJoystick;
+    private Rigidbody2D rig;
+    private Animator anim;
+    float moveH, moveV, speed = 5;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        MovePlayer();            
+    }
+
+    void MovePlayer()
+    {
+        moveH = moveJoystick.Horizontal;
+        Vector2 dir = new Vector2(moveH, 0);
+        rig.velocity = new Vector2(moveH * speed, 0);
+    }
+}
